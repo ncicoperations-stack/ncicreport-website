1523d307e638f7931b5afeaeec9eac4da1c91fc2
ncicoperations-stack <ncic.operations@gmail.com>
2026-07-31 11:17:19 -0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,17 @@
 window.NCIC_CONFIG = {
+
   siteName: "National Cyber Intelligence Center",
+
   reportPrefix: "NCIC",
+
   storageKey: "ncic-draft-report",
+
+  api: {
+    endpoint: "https://script.google.com/macros/s/AKfycbxBBkJHa9tGFMN1O0JppeArWNhRlvaR_mIDLIJZYnrDcD2muisddKT3H6WGi_2_1nST/exec",
+    timeout: 30000,
+    retries: 2
+  },
+
+  debug: false
+
 };
